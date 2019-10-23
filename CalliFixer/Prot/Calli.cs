5cd20cab941db178c94483813e78b1f1bd4f1544
Rzy <45715088+Riziebtw@users.noreply.github.com>
2019-10-23 22:14:52 +0200

    Update Calli.cs

--- CalliFixer/Prot/Calli.cs
+++ CalliFixer/Prot/Calli.cs
@@ -26,7 +26,7 @@ namespace CalliFixer.Protections
 
                         if (instructions[i].OpCode == OpCodes.Ldftn)
                         {
-                            instructions[i].OpCode = OpCodes.Call;
+                            instructions[i].OpCode = OpCodes.Callvirt;
                            
 
                             decrypted++;
